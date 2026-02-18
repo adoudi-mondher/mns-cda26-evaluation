1f7ceb638c6fa7d62a3224143c9ec56d6efa519e
Mondher Adoudi <adoudi.mondher@ik.me>
2026-02-18 16:31:22 +0100

    feat: add Jinja2 templates and fix JSON encoding

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+app.config['JSON_AS_ASCII'] = False
 
 db = SQLAlchemy(app)
 
-from models import Event
+from routes import * 
 
 with app.app_context():
+    from models import Event
     db.create_all()
 
 if __name__ == "__main__":
